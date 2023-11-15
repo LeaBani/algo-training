f3c87f19986440bdfbacddd66d01e05d6d48755a
LeaBani <113905453+LeaBani@users.noreply.github.com>
2023-11-15 17:59:40 +0100

    tuple method

--- fizzBuzz.py
+++ fizzBuzz.py
@@ -2,15 +2,14 @@ def reverse_fizz_buzz(list_of_numbers):
     new_list_of_numbers = []
 
     for index, char in enumerate(list_of_numbers):
-        if char == "Fizz":
-            res = index+1
-            new_list_of_numbers.append(res)
-        elif char == "Buzz":
+        if char == "Fizz" or char == "Buzz":
             res = index+1
             new_list_of_numbers.append(res)
 
-            print(new_list_of_numbers)
+    print(tuple(new_list_of_numbers))
 
-list_of_numbers = [1,"FizzBuzz",3,"FizzBuzz",5,"FizzBuzz"]
+    return tuple(new_list_of_numbers) # tuple permet de représenter une paire ordonnée de valeurs en Python
+
+list_of_numbers = [1,2,"Fizz",4,"Buzz"]
 resultat = reverse_fizz_buzz(list_of_numbers)    
 print(resultat) 
